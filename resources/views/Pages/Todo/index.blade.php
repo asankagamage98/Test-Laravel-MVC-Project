b2a65fa0ee5ac191e25f7fe26cbb171539d89149
asankagamage98 <asankagamage1998@gmail.com>
2023-10-17 23:05:37 +0530

    create controllers and home api

---
+++ resources/views/Pages/Todo/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+@include('Libraries.style')
+
+<body>
+
+    @include('Libraries\script')
+    <h1>
+        Todo view page
+    </h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//home
+Route::get('/',[HomeController::class,'index'])->name('Home');
+//todo
+Route::get('/todo',[TodoController::class,'index'])->name('Todo');
